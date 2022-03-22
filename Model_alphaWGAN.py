c5f67a021c27329e44bc3ee72c9fc3a232ceb723
jrmkim50 <jrmkim50@gmail.com>
2022-03-22 12:53:22 -0400

    modified alphagan and wgan to use my sizes

--- Model_alphaWGAN.py
+++ Model_alphaWGAN.py
@@ -9,35 +9,51 @@ from torch.nn import functional as F
 #Encoder and Discriminator has same architecture
 #***********************************************
 class Discriminator(nn.Module):
-    def __init__(self, channel=512,out_class=1,is_dis =True):
-        super(Discriminator, self).__init__()
-        self.is_dis=is_dis
+    def __init__(self, channel=512, resolution=64):
+        super(Discriminator, self).__init__()        
         self.channel = channel
-        n_class = out_class 
+        self._resolution = resolution
         
         self.conv1 = nn.Conv3d(1, channel//8, kernel_size=4, stride=2, padding=1)
+        self.bn1 = nn.BatchNorm3d(channel//8)
         self.conv2 = nn.Conv3d(channel//8, channel//4, kernel_size=4, stride=2, padding=1)
         self.bn2 = nn.BatchNorm3d(channel//4)
         self.conv3 = nn.Conv3d(channel//4, channel//2, kernel_size=4, stride=2, padding=1)
         self.bn3 = nn.BatchNorm3d(channel//2)
         self.conv4 = nn.Conv3d(channel//2, channel, kernel_size=4, stride=2, padding=1)
         self.bn4 = nn.BatchNorm3d(channel)
-        self.conv5 = nn.Conv3d(channel, n_class, kernel_size=4, stride=1, padding=0)
-        
-    def forward(self, x, _return_activations=False):
-        h1 = F.leaky_relu(self.conv1(x), negative_slope=0.2)
-        h2 = F.leaky_relu(self.bn2(self.conv2(h1)), negative_slope=0.2)
-        h3 = F.leaky_relu(self.bn3(self.conv3(h2)), negative_slope=0.2)
-        h4 = F.leaky_relu(self.bn4(self.conv4(h3)), negative_slope=0.2)
-        h5 = self.conv5(h4)
-        output = h5
-        
-        return output
+        self.conv5 = nn.Conv3d(channel, 1, kernel_size=4, stride=2, padding=1)
+        self.leaky_relu = nn.LeakyReLU(0.2)
+
+        _inchannels = 2*2*5 if self._resolution == 64 else 2*2*5*512
+        self.dense1 = nn.Linear(_inchannels, channel)
+        self.dense2 = nn.Linear(channel, 1)
+
+    def forward(self, x):
+        # input: 64,64,160 - conv1 -> 32,32,80 - conv2 -> 16,16,40 - conv3 -> 8,8,20
+        #     - conv4 -> 4,4,10 - conv5 -> 2,2,5 - dense1 -> 512 - dense2 -> 1
+        # input: 32,32,80 - conv1 -> 16,16,40 - conv2 -> 8,8,20 - conv3 -> 4,4,10
+        #     - conv4 -> 2,2,5 - dense1 -> 512 - dense2 -> 1
+        h1 = self.leaky_relu(self.bn1(self.conv1(x)))
+        h2 = self.leaky_relu(self.bn2(self.conv2(h1)))
+        h3 = self.leaky_relu(self.bn3(self.conv3(h2)))
+        if self._resolution == 64:
+            h4 = self.leaky_relu(self.bn4(self.conv4(h3)))
+            h5 = self.conv5(h4)
+            h5 = h5.view(-1)
+            h5 = self.leaky_relu(self.dense1(h5))
+            h5 = self.dense2(h5)
+            return h5
+        else:
+            h4 = self.conv4(h3)
+            h4 = h4.view(-1)
+            h4 = self.leaky_relu(self.dense1(h4))
+            h4 = self.dense2(h4)
+            return h4
     
 class Code_Discriminator(nn.Module):
     def __init__(self, code_size=100,num_units=750):
         super(Code_Discriminator, self).__init__()
-        n_class = 1
         self.l1 = nn.Sequential(nn.Linear(code_size, num_units),
                                 nn.BatchNorm1d(num_units),
                                 nn.LeakyReLU(0.2,inplace=True))
@@ -51,51 +67,66 @@ class Code_Discriminator(nn.Module):
         h2 = self.l2(h1)
         h3 = self.l3(h2)
         output = h3
-            
         return output
 
 class Generator(nn.Module):
-    def __init__(self, noise:int=100, channel:int=64):
+    def __init__(self, noise:int=1000, channel:int=64, resolution=64):
         super(Generator, self).__init__()
         _c = channel
-
-        self.relu = nn.ReLU()
+        self._resolution = resolution
+        
         self.noise = noise
-        self.tp_conv1 = nn.ConvTranspose3d(noise, _c*8, kernel_size=4, stride=1, padding=0, bias=False)
+        self.fc = nn.Linear(self.noise,512*2*2*5)
         self.bn1 = nn.BatchNorm3d(_c*8)
         
-        self.tp_conv2 = nn.Conv3d(_c*8, _c*4, kernel_size=3, stride=1, padding=1, bias=False)
+        self.tp_conv2 = nn.Conv3d(_c*8, _c*4, kernel_size=3, stride=1, padding=1)
         self.bn2 = nn.BatchNorm3d(_c*4)
         
-        self.tp_conv3 = nn.Conv3d(_c*4, _c*2, kernel_size=3, stride=1, padding=1, bias=False)
+        self.tp_conv3 = nn.Conv3d(_c*4, _c*2, kernel_size=3, stride=1, padding=1)
         self.bn3 = nn.BatchNorm3d(_c*2)
         
-        self.tp_conv4 = nn.Conv3d(_c*2, _c, kernel_size=3, stride=1, padding=1, bias=False)
+        self.tp_conv4 = nn.Conv3d(_c*2, _c, kernel_size=3, stride=1, padding=1)
         self.bn4 = nn.BatchNorm3d(_c)
         
-        self.tp_conv5 = nn.Conv3d(_c, 1, kernel_size=3, stride=1, padding=1, bias=False)
+        self.tp_conv5 = nn.Conv3d(_c, 1, kernel_size=3, stride=1, padding=1)
+        if self._resolution == 64:
+            self.tp_conv5 = nn.Sequential(
+                nn.Conv3d(_c, _c, kernel_size=3, stride=1, padding=1),
+                nn.BatchNorm3d(_c),
+                nn.ReLU(),
+                nn.Upsample(scale_factor=2),
+                nn.Conv3d(_c, 1, kernel_size=3, stride=1, padding=1)
+            )
+        
+        self.relu = nn.ReLU()
+        self.tanh = nn.Tanh()
+        self.upsample = nn.Upsample(scale_factor=2)
         
     def forward(self, noise):
-
-        noise = noise.view(-1,self.noise,1,1,1)
-        h = self.tp_conv1(noise)
+        # 1000 - fc -> 512,2,2,5 - conv2 -> 256,4,4,10 - conv3 -> 128,8,8,20 
+        #     - conv4 -> 64,16,16,40 - conv5 -> 64,32,32,80 - conv6 -> 1,64,64,160
+        # 1000 - fc -> 512,2,2,5 - conv2 -> 256,4,4,10 - conv3 -> 128,8,8,20 
+        #     - conv4 -> 64,16,16,40 - conv5 -> 1,32,32,80
+        noise = noise.view(-1, self.noise)
+        h = self.fc(noise)
+        h = h.view(-1,512,2,2,5)
         h = self.relu(self.bn1(h))
-        
-        h = F.upsample(h,scale_factor = 2)
+
+        h = self.upsample(h,scale_factor = 2)
         h = self.tp_conv2(h)
         h = self.relu(self.bn2(h))
-     
-        h = F.upsample(h,scale_factor = 2)
+        
+        h = self.upsample(h,scale_factor = 2)
         h = self.tp_conv3(h)
         h = self.relu(self.bn3(h))
 
-        h = F.upsample(h,scale_factor = 2)
+        h = self.upsample(h,scale_factor = 2)
         h = self.tp_conv4(h)
         h = self.relu(self.bn4(h))
 
-        h = F.upsample(h,scale_factor = 2)
+        h = self.upsample(h,scale_factor = 2)
         h = self.tp_conv5(h)
 
-        h = F.tanh(h)
+        h = self.tanh(h)
 
         return h
